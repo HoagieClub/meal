bd0ddae8c95016dc3dcbf636bfad8d03813895ea
Windsor <mn4560@princeton.edu>
2024-10-27 00:09:25 -0400

    add: requirements.txt

--- backend/hoagiemeal/models.py
+++ backend/hoagiemeal/models.py
@@ -1,3 +1,20 @@
+"""Web scraper for extracting and structuring menu item information.
+
+Copyright © 2021-2024 Hoagie Club and affiliates.
+
+Licensed under the MIT License. You may obtain a copy of the License at:
+
+    https://github.com/hoagieclub/meal/blob/main/LICENSE
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, subject to the following conditions:
+
+This software is provided "as-is", without warranty of any kind.
+"""
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
