92190e5578dace81178c33c871afa245e405c120
Caleb <uongcaleb@gmail.com>
2025-09-27 15:10:49 -0400

    add contributor

--- frontend/app/feature3/page.tsx
+++ frontend/app/feature3/page.tsx
@@ -78,11 +78,12 @@ const teamLeads = [
 const teamMembers = [
   // Add your name, role, and image here!
   {
-    name: 'John Doe',
-    role: 'Frontend Developer',
-    imgSrc: 'https://placehold.co/100x100/d1fae5/059669?text=JD',
+    name: 'Caleb Kha-Uong',
+    role: 'Fullstack Developer',
+    imgSrc: 'https://media.licdn.com/dms/image/v2/D5603AQHtWsVjQbCx-g/profile-displayphoto-crop_800_800/B56ZhC4LH1HUAI-/0/1753468679847?e=1761782400&v=beta&t=K_223NrmbY7pDgpaFeY-HFgf9DDm_EOqZs9T-IysviE',
     socials: {
-      linkedin: 'https://www.linkedin.com/in/abuahmed0821/',
+      linkedin: 'https://www.linkedin.com/in/calebK25/',
+      website: 'https://caleb-k.com',
     },
   },
 ];
