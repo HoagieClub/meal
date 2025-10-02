40f6cc574e351ffbbbb98d2f88f254fe54a2b81b
MalachiNoel3 <malachidnoel@gmail.com>
2025-10-01 20:41:31 -0400

    Adding Malachi Noel to contributors

--- frontend/app/feature3/page.tsx
+++ frontend/app/feature3/page.tsx
@@ -78,11 +78,11 @@ const teamLeads = [
 const teamMembers = [
   // Add your name, role, and image here!
   {
-    name: 'John Doe',
-    role: 'Frontend Developer',
-    imgSrc: 'https://placehold.co/100x100/d1fae5/059669?text=JD',
+    name: 'Malachi Noel',
+    role: 'Developer',
+    imgSrc: 'https://i.imgur.com/MPwu9Nc.jpeg',
     socials: {
-      linkedin: 'https://www.linkedin.com/in/abuahmed0821/',
+      linkedin: 'https://www.linkedin.com/in/malachi-noel/',
     },
   },
 ];
