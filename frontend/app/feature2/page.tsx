baa030db2a9fc6f6d5c2042175afed3d4215005d
Spencer04Hire <124114480+Spencer04Hire@users.noreply.github.com>
2025-10-21 16:17:02 -0400

    Fix more types

--- frontend/app/feature2/page.tsx
+++ frontend/app/feature2/page.tsx
@@ -932,7 +932,7 @@ export default function DietPlanner() {
         padding={majorScale(3)}
         backgroundColor='white'
         boxShadow='0px 10px 15px -3px rgba(0,0,0,0.07), 0px 4px 6px -2px rgba(0,0,0,0.05)'
-        onSubmit={(e) => {
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
           e.preventDefault();
           handleGeneratePlan(currentDateObj);
         }}
