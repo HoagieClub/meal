7cd8366162cf5b1762e68d9e84bd47c5a215fe88
adb8 <abuahmed0821@gmail.com>
2025-11-22 01:19:23 -0500

    .

--- backend/hoagiemeal/settings.py
+++ backend/hoagiemeal/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1", "t")
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
